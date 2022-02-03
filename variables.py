a2a89353b4da8a4c1620491325e581f8a2780a9a
Jack Smith <Jackjaysmith6@gmail.com>
2022-02-02 18:30:27 -0700

    Made some print changes

--- variables.py
+++ variables.py
@@ -2,3 +2,13 @@
 # Variables in Python
 # By Jack
 # CS1030
+
+# Variables
+
+first_name = 'Jack'
+last_name = 'Smith'
+
+print (first_name, last_name)
+print (first_name + last_name)
+print ('Mr.' + first_name + '' + last_name)
+
